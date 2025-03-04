8b09450b8fbee53052c599bd33e7e6ee14681fb8
waninger <mikael.waninger@gmail.com>
2025-03-04 14:10:47 +0100

    mangum install for aws lambda

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, World! update"}
+
+handler = Mangum(app)
